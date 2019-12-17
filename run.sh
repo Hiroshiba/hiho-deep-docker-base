70cb446062fc5a867daa84d0c1be0e8e198a9671
Hiroshiba Kazuyuki <kazuyuki_hiroshiba@dwango.co.jp>
2019-12-17 22:19:43 +0900

    add some

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 set -eu
 
-for dockerfile in Dockerfile_*; do
+target=${1:-'*'}
+
+for dockerfile in Dockerfile_$target; do
   tag=${dockerfile/Dockerfile_/}
   docker build -f $dockerfile -t hiroshiba/hiho-deep-docker-base:$tag .
-  docker push hiroshiba/hiho-deep-docker-base:$tag
+  docker push "hiroshiba/hiho-deep-docker-base:$tag"
 done
